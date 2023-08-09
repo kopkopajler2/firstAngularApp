085cefb9b26b0b29b1be5aae3628c6776a00aa1f
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-08-09 15:42:01 -0400

    App updated to use route parameters to pass data, and routerLink to use dinamic data to create a route and show specific information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,16 @@ import { RouterModule } from '@angular/router';
   ],
   template: `
   <main>
-  <a [routerLink]="['/']">
-  <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-  </a>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
       <router-outlet></router-outlet>
     </section>
   </main>
-  `,
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
