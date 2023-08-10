c21e106c95e20af1e14d784b0bbdc1a31e14db2a
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-08-09 20:42:41 -0400

    local web server json-server and asynchronous service methods to retrieve data. Img from api are not founded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -36,8 +36,11 @@ export class HomeComponent {
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList : HousingLocation[])=> {
+
+      this.housingLocationList = housingLocationList ;
+      this.filteredLocationList = housingLocationList;
+    })
   }
   filterResults(text: string) {
     if (!text) {
